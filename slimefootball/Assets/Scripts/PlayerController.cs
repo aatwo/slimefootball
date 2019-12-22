81ec20e15dba77fe1f4a1d17ed2e61bdf8ce021d
Aaron Wilson <aaron.wi@gmail.com>
2019-12-22 17:44:54 +0000

    added a jump mechanic. Jump is floaty but then satisfyingly fast when falling. Jump can only happen once per button press.

--- slimefootball/Assets/Scripts/PlayerController.cs
+++ slimefootball/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,20 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     int playerIndex = 0;
 
+    enum JumpState
+    {
+        can_jump,
+        jumping,
+        falling
+    };
+
+    JumpState jumpState = JumpState.can_jump;
+    bool hasReleasedJumpSinceLastJump = true;
+
+    float jumpForceDurationS = 0.3f;
+    float jumpStartTime = 0f;
+    float jumpForcePerSecond = 1000f;
+
     public void SetPlayerIndex(int n)
     {
         playerIndex = n;
@@ -40,8 +54,10 @@ public class PlayerController : MonoBehaviour
     void ProcessKeyboardInput()
     {
         string horizontalAxisName = "Horizontal" + playerIndex + "_key";
-        //string verticalAxisName = "Vertical" + playerIndex + "_key"; // TODO: JUMP
+        string verticalAxisName = "Vertical" + playerIndex + "_key";
+
         ProcessHorizontalAxisInput( horizontalAxisName );
+        ProcessVerticalAxisInput( verticalAxisName );
     }
 
     void ProcessHorizontalAxisInput( string horizontalName )
@@ -50,5 +66,88 @@ public class PlayerController : MonoBehaviour
         float xVel = horizontalInput * maxSpeed;
 
         rb.velocity = new Vector2( xVel, rb.velocity.y );
+    }
+
+    void ProcessVerticalAxisInput( string verticalName )
+    {
+        if( playerIndex != 0 )
+            return;
+
+        float verticalInput = Input.GetAxisRaw(verticalName );
+
+        // Jump rules:
+        //      1. if player is on floor they can press jump
+        //      2. while holding jump jump force will be applied
+        //      3. after releasing jump jump force stops
+        //      4. player can't jump again until landed
+        //      5. jump force only applied for a limited time
+        //      6. after a successful jump the user cannot jump until they release and press it again
+
+        bool pressingJump = (verticalInput > 0.0f);
+        if( !pressingJump )
+            hasReleasedJumpSinceLastJump = true;
+
+        switch(jumpState)
+        {
+            case JumpState.can_jump:
+            {
+                if( pressingJump && hasReleasedJumpSinceLastJump )
+                {
+                    hasReleasedJumpSinceLastJump = false;
+                    jumpStartTime = Time.time;
+                    jumpState = JumpState.jumping;
+                    rb.AddForce( new Vector2( 0f, 150f ) );
+                }
+                break;
+            }
+
+            case JumpState.jumping:
+            {
+                // Check if player let go of jump
+                if( !pressingJump )
+                {
+                    jumpState = JumpState.falling;
+                }
+                else
+                {
+                    // Check how long we've been pressing jump for
+                    float jumpPressDuration = Time.time - jumpStartTime;
+                    if( jumpPressDuration >= jumpForceDurationS )
+                    {
+                        jumpState = JumpState.falling;
+                    }
+                    else
+                    {
+                        // Continue applying jump force
+                        float forceToApply = (Time.deltaTime * jumpForcePerSecond);
+                        rb.AddForce( new Vector2( 0f, forceToApply ) );
+                    }
+                }
+                break;
+            }
+
+            case JumpState.falling:
+            {
+                // When falling we apply extra downwards force on top of gravity to make
+                // it feel more satisfying. I.E floaty upwards jump followed by a fast
+                // satisfying fall.
+                float forceToApply = -0.5f * (Time.deltaTime * jumpForcePerSecond);
+                rb.AddForce( new Vector2( 0f, forceToApply ) );
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        jumpState = JumpState.can_jump;
+    }
+
+    private void OnTriggerStay2D( Collider2D collision )
+    {
+        if( jumpState == JumpState.falling )
+        {
+            jumpState = JumpState.can_jump;
+        }
     }
 }
